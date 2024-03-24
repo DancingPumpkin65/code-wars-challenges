2326d4e8a50854a3827be425ac56d549848bf794
Scripted Logic <149482797+DancingPumpkin65@users.noreply.github.com>
2024-03-24 20:36:23 +0000

    Update solution.php

--- PHP/IPv4 Validator/solution.php
+++ PHP/IPv4 Validator/solution.php
@@ -1,21 +1,9 @@
 <?php
 function isValidIP(string $str): bool
 {
-    // Check if the string matches the IPv4 format ^^
-    if (!preg_match('/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/', $str, $matches)) {
+    // Check if the string matches the IPv4 format :)
+    if (!preg_match('/^(0|[1-9]\d?|1\d\d|2[0-4]\d|25[0-5])\.(0|[1-9]\d?|1\d\d|2[0-4]\d|25[0-5])\.(0|[1-9]\d?|1\d\d|2[0-4]\d|25[0-5])\.(0|[1-9]\d?|1\d\d|2[0-4]\d|25[0-5])$/', $str)) {
         return false;
-    }
-    
-    // Check if each octet is valid :/
-    foreach (array_slice($matches, 1) as $octet) {
-        if (!filter_var($octet, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0, 'max_range' => 255]])) {
-            return false;
-        }
-        
-        // Check for leading zeros :0
-        if ($octet !== '0' && $octet[0] === '0') {
-            return false;
-        }
     }
     
     return true;
