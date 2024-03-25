bd11f31a53643c1632401eff18358d0795e8a78e
Scripted Logic <149482797+DancingPumpkin65@users.noreply.github.com>
2024-03-25 01:38:01 +0000

    Update solution.php

--- PHP/DNA Complement/solution.php
+++ PHP/DNA Complement/solution.php
@@ -1,6 +1,8 @@
 <?php
 function DNA_strand($dna) {
+    // Convert the DNA string into an array of characters
     $dna0 = str_split($dna);
+    // 
     for ($i = 0; $i < count($dna0); $i++) {
         $dna0[$i] = ($dna0[$i] == "A") ? "T" : (($dna0[$i] == "T") ? "A" : (($dna0[$i] == "C") ? "G" : (($dna0[$i] == "G") ? "C" : $dna0[$i])));
     }
